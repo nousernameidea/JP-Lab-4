4e4d9fe8d5c62b4a38ec9c6beb8b89cd198970af
nousernameidea <nousernameidea@users.noreply.github.com>
2017-11-23 14:36:09 +0100

    first version

---
+++ src/figury/Elipsa.java
@@ -0,0 +1,19 @@
+package figury;
+
+import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Area;
+import java.awt.geom.Ellipse2D;
+
+public class Elipsa extends Figura {
+
+	public Elipsa(Graphics2D buf, int del, int w, int h) {
+		super(buf, del, w, h);
+		
+		shape = new Ellipse2D.Float(0, 0, w/3, h/3);
+        aft = new AffineTransform();                                  
+        area = new Area(shape);
+		// TODO Auto-generated constructor stub
+	}
+
+}
